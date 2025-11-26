c4330e5005f7bbe09fe641331dc6c90b8fcf3081
LugnaDash3 <lugnadash3@gmail.com>
2025-11-26 15:26:08 +0530

    Create OneWin.java

---
+++ OneWin.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+class OneWin
+{ 
+   public static void main (String args[])
+   {  
+      Frame f = new Frame();
+         f.setSize(300,200);
+         f.setBackground(Color.red);
+         f.setVisible(true);
+    }
+}
