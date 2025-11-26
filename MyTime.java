f09c7687b1e201dde3a5df58654b26907ceb1769
LugnaDash3 <lugnadash3@gmail.com>
2025-11-26 15:22:50 +0530

    Create  MyTime.java

---
+++ MyTime.java
@@ -0,0 +1,8 @@
+class MyTime
+{
+    public static void main(String[] args) 
+    {
+        java.util.Date d = new java.util.Date();
+        System.out.println(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+    }
+}
