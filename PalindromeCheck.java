69ae8ed37f703f2e9bbce0a7e648cf072e820eca
LugnaDash3 <lugnadash3@gmail.com>
2025-11-26 15:36:46 +0530

    Create PalindromeCheck.java

---
+++ PalindromeCheck.java
@@ -0,0 +1,19 @@
+public class PalindromeCheck 
+{
+    public static void main(String[] args) 
+{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        str = str.toLowerCase();
+        String reversed = new    StringBuilder(str).reverse().toString();
+        if (str.equals(reversed)) 
+{
+            System.out.println("The string is a palindrome.");
+        } 
+else 
+{
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+}
