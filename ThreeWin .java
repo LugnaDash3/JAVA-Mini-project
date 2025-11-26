30e66c9655529ced0fd011ed956ba4e9caa1cb50
LugnaDash3 <lugnadash3@gmail.com>
2025-11-26 15:28:20 +0530

    Create ThreeWin .java

---
+++ ThreeWin .java
@@ -0,0 +1,19 @@
+import java.awt.*;
+class ThreeWin 
+ {
+    public static void main(String args[]) 
+{
+        Frame f1 = new Frame("Red Window");
+        f1.setSize(300, 200);
+        f1.setLocation(0, 0);  f1.setBackground(Color.red);
+        f1.setVisible(true);
+        Frame f2 = new Frame("Blue Window");
+        f2.setSize(300, 200);
+       f2.setLocation(1243, 0);  f2.setBackground(Color.blue);
+        f2.setVisible(true);
+        Frame f3 = new Frame("Yellow Window");
+        f3.setSize(300, 200);
+        f3.setLocation(0, 620);  f3.setBackground(Color.yellow);
+        f3.setVisible(true);
+    }
+}
